0443124baf20bba6066d6737c7e203a59fbdff1b
ricky8221 <110708834+ricky8221@users.noreply.github.com>
2022-11-07 14:48:32 -0800

    front-end fixed

---
+++ client/src/pages/game.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CodeNames from '../components/codeNames'
+
+
+function Game() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CodeNames />
+      </header>
+    </div>
+  );
+}
+
+export default Game;
