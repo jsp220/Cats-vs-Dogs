4336960984914bb711679310565c2f349c5ecd1a
BrianZoulko <bzoulko@gmail.com>
2022-11-07 10:35:06 -0800

    Initialized Codenames

--- src/pages/rules.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Rules = () => {
-  return (
-    <div className='rules'>rules</div>
-  )
-}
-
-export default Rules;
--- src/pages/team2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Team2 = () => {
-  return (
-    <div className='right'>team2</div>
-  )
-}
-
-export default Team2;
