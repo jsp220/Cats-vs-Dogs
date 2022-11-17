eea72f708956283ee827bf7414f3f9c9f685aead
ricky8221 <110708834+ricky8221@users.noreply.github.com>
2022-11-17 11:35:45 -0800

    fixing the component spelling problem

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import PrivateRouteNoToken from "./components/PrivateRouteNoToken";
 import PrivateRouteWithToken from "./components/PrivateRouteWithToken";
 import Navbar from "./components/Navbar";
 import Game from "./pages/game";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
