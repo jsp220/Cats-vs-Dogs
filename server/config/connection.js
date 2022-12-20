b907a9f5365ba96fbffab94476518f0b7f77fd0a
Joon Park <jsp220@gmail.com>
2022-12-19 17:00:59 -0800

    update env variable to MONGO_URI

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/gameDB', {
+mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/gameDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
