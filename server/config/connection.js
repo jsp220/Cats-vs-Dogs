8fb38a51865882b2d57c675a7b5d435945d069fc
Joon Park <jsp220@gmail.com>
2022-12-19 17:17:19 -0800

    change env variable for connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/gameDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/gameDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 module.exports = mongoose.connection;
+
+
+// username: joonpark
